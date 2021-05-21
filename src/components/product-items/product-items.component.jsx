d6351c74bd93aaf5f4c4afe8492860d740038084
Resume Website <jdcollins242@gmail.com>
2021-05-20 23:08:19 -0400

    Added collection 480px view. Added product page 480px view

--- src/components/product-items/product-items.component.jsx
+++ src/components/product-items/product-items.component.jsx
@@ -1,28 +1,31 @@
-import React from 'react';
-import CollectionItem from '../collection-item/collection-item.component'
+import React from "react";
+import CollectionItem from "../collection-item/collection-item.component";
 // Style Imports
-import './product-items.style.scss'
+import "./product-items.style.scss";
 
-import SHOP_DATA from '../../pages/ShopPage/shop.data';
+import SHOP_DATA from "../../pages/ShopPage/shop.data";
 
 import { withRouter } from "react-router-dom";
-
 
 function ProductItems({ products }) {
     return (
         <div className="TablesPage ProductPage container">
-            {products.items.map((item) => {
-                return (
-                    <div className="ProductPage-item">
-                        <CollectionItem
-                            items={products}
-                            collectionItem={item}
-                        />
-                    </div>
-                );
-            })}
+            <h1 className="ProductPage-title">{products.title}</h1>
+            <div className="ProductPage-container">
+                {products.items.map((item) => {
+                    return (
+                        <div className="ProductPage-item">
+                            <CollectionItem
+                                items={products}
+                                collectionItem={item}
+                            />
+                        </div>
+                    );
+                    console.log(products);
+                })}
+            </div>
         </div>
     );
 }
 
-export default ProductItems
+export default ProductItems;
